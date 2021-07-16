e47b5fc5c393df7851747a1663968642dd3478fe
Lyncoln <lyncolnsousa@id.uff.br>
2021-07-16 00:53:02 -0300

    arrumacao dos scripts e stream

--- kibana/python-to-kibana.py
+++
@@ -1,8 +0,0 @@
-from elasticsearch import helpers, Elasticsearch
-import csv
-
-es = Elasticsearch()
-
-with open('Date=2004-10-01.csv') as f:
-    reader = csv.DictReader(f)
-    helpers.bulk(es, reader, index='my-index', doc_type='my-type')
